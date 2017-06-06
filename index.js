73414c838856778909808b27912e67d6eabd0254
Nathaniel-j-s <nathaniel.j.mith@gmail.com>
2017-06-06 00:30:13 -0600

    fixing endpoint structure

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const mainCtrl = {
         res.send(result);
       }
     });
-  },
+  }, // Figure out GROUP BY, then use it.
   listGames: function(req, res, next) {
     db.characters.listGames([],
     function(err, result) {
@@ -114,13 +114,13 @@ const mainCtrl = {
 // Massive JS function calls below. Again, bad practice, sorry! So un-modularized!
 app.get('/api/characters', mainCtrl.listChars);
 app.get('/api/games', mainCtrl.listGames);
-app.get('/api/charactersbygame', mainCtrl.listCharsByGame);
-app.get('/api/sortgamesbyfun', mainCtrl.sortGamesByFun);
-app.get('/api/character', mainCtrl.selectCharacter);
+app.get('/api/characters/:games', mainCtrl.listCharsByGame);
+app.get('/api/games/fun', mainCtrl.sortGamesByFun); // <-- Fix these things. No verbs.
+app.get('/api/character/:charid', mainCtrl.selectCharacter);
 app.post('/api/newcharacter', mainCtrl.newCharacter);
 app.post('/api/newgame', mainCtrl.newGame);
-app.put('/api/updatecharacter', mainCtrl.updateCharacter);
-app.delete('/api/deletecharacter', mainCtrl.deleteCharacter);
+app.put('/api/characters', mainCtrl.updateCharacter); // URL looks like http://localhost:3876/api/characters?charid=1. Figure out why /:charid is breaking this line.
+app.delete('/api/characters/:charid', mainCtrl.deleteCharacter);
 
 // The usual.
 app.listen(3876, function() {
